4d289406c09da2ea5fc7d930b189cd436b80a3cb
cosasfabulosas <david.velaz@unir.net>
2024-12-05 13:07:03 +0100

    Add files via upload

---
+++ Script1_UNIR.R
@@ -0,0 +1,123 @@
+########################################################
+########################################################
+##                                                    ##
+##          CURSO INICIACIÓN AL LENGUAJE R            ##
+##                                                    ##
+##                     Script  1                      ##
+##                                                    ##
+########################################################
+########################################################
+
+# Primera cosa que vemos la # que me sirve para introducir notas
+
+# Vamos con algo muy simple: escribimos 1+1 y ejecutamos RUN
+
+1+1
+
+# Vamos con algunas funciones básicas de R
+
+# Funciones matemáticas en R
+# Operadores: =, <-, ==, +, etc.
+
+5 + 5
+10-5
+10*4532
+45320 / 10
+10*pi
+
+# Raíz cuadrada
+sqrt(144)
+
+#Para las potencias
+2^3
+
+#Valor absoluto
+abs(-4)
+
+#Redondear
+round(1.8)
+
+# Vemos como en la consola tenemos un línea con la operación 1+2
+# y luego el resultado 2 con un número precedido de 2 corchetes
+# [1] 2. El número entre corchete [1] no es el resultado, sino el 
+# número índice del primer elemento de cada línea nueva. Ese número 
+# puede incrementarse como verás.
+
+# Para entender los índices
+10:100
+
+# ¿Qué pasa si tecleas una orden incompleta como 3-?
+
+#Crear variables
+x = 10
+10 -> x
+x <- 10
+
+# Inspeccionar una variable
+x
+
+# Algunas pruebas
+x-2
+x
+y = 5
+x + y
+x
+y
+z = x + y
+z
+
+y == x        # Comparar x con y. "==" (igualdad) "!=" (desigualdad)
+
+# Creo variables con texto
+
+k = "HELLO WORLD!"
+
+VariableMicrorrelato = "Cuando despertó, el dinosaurio todavía estaba allí"
+
+# Función en R
+
+print(k) # imprimir en pantalla la variable
+print("HELLO WORLD!")
+
+nchar(VariableMicrorrelato) #contar el número de caracteres
+
+# Para acceder a la documentación
+help()
+help(sqrt)
+
+#Lista de variables
+ls()
+
+#Eliminar una variable
+rm(z)
+
+# Directorio de trabajo
+
+# Crear un directorio de trabajo
+
+# Para Windows
+dir.create("C:/seminarioR")
+#Para Mac
+dir.create("Desktop/seminarioR")
+
+# Fijar el directorio de trabajo
+
+# En Windows
+setwd("C:/seminarioR") # ruta donde están los archivos con los que quiero trabajar
+
+#En Mac
+setwd("Desktop/seminarioR") # ruta donde están los archivos con los que quiero trabajar
+
+# Para saber en qué directorio estoy
+
+getwd()
+
+# Coloca dentro de la carpeta "seminarioR" que acabas de crear
+# los documentos txt que has descargado de GitHub
+
+#################################################################
+#               Ejercicio1:                                     #
+# Si a vale 546 y b vale 328, ¿la suma de ambos es igual a 844? #
+#            Debes llegar hasta TRUE                            #
+# Cuenta el número de caracteres de "HELLO WORLD!"              #
+#################################################################
---
+++ Script2_UNIR.R
@@ -0,0 +1,137 @@
+########################################################
+########################################################
+##                                                    ##
+##                   INICIACIÓN A R                   ##
+##                                                    ##
+##                     Script  2                      ##
+##                                                    ##
+########################################################
+########################################################
+
+# Algunas funciones y comandos vienen por defecto en R,
+# pero la mayoría hay que instalarlos:
+
+# Instalar paquetes (solo la primera vez)
+install.packages("tokenizers")
+install.packages("tidyverse")
+install.packages("tidytext")
+
+#Cargar los paquetes que vamos a usar en cada sesión con la función library()
+library(tokenizers)
+library(tidyverse)
+library(tidytext)
+
+# Información sobre el paquete y sus funciones
+?tidyverse
+
+# Primera prueba
+
+texto <- "En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lantejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda. El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de lo mesmo, y los días de entresemana se honraba con su vellorí de lo más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años; era de complexión recia, seco de carnes, enjuto de rostro, gran madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada, o Quesada, que en esto hay alguna diferencia en los autores que deste caso escriben; aunque, por conjeturas verosímiles, se deja entender que se llamaba Quejana. Pero esto importa poco a nuestro cuento; basta que en la narración dél no se salga un punto de la verdad."
+palabras <- tokenize_words(texto)
+palabras
+
+# Leer un archivo de texto en R
+
+# Windows
+soneto = readLines("Soneto.txt", encoding = "UTF-8")
+
+# Mac
+soneto = readLines("Soneto.txt")
+soneto
+
+# ¿Cuántos elementos tiene la variable soneto?
+length(soneto)
+
+# ¿Qué tipo de elementos son?
+typeof(soneto)
+
+# Si me interesa el primer verso
+soneto[1]
+
+# Si me interesa el último verso
+soneto[14]
+
+# Para recuperar varios elementos a la vez
+soneto[c(1,14)]
+
+###########################
+# Primer análisis textual #
+###########################
+
+# Crear una tabla con información del texto
+
+poema <- tibble(línea = seq_along(soneto),
+                  verso = soneto)
+poema
+
+# Número de columnas
+length(poema)
+# Número de filas
+nrow(poema)
+class(poema)
+
+# Extraigo las palabras
+poema_palabras <- poema %>%
+  unnest_tokens(palabra, verso)
+poema_palabras
+
+# PALABRAS TIPO Y PALABRAS TOKENS #
+
+# ¿Cuántas palabras tipo hay?
+poema_palabras %>%
+  count(palabra)
+
+# Para ordenarlas por frecuencia
+
+poema_palabras_frec_abs = poema_palabras %>%
+  count(palabra,
+        sort = TRUE)
+
+poema_palabras_frec_abs
+
+# Contar el número de caracteres: añadimos una columna con la función mutate()
+
+poema_palabras_con_caracteres <- poema_palabras %>%
+  mutate(NumLetras = nchar(palabra))
+
+poema_palabras_con_caracteres
+
+# ¿Cuántas palabras hay con un número de letras en concreto?
+poema_palabras_con_caracteres %>%
+  count(NumLetras)
+
+# Ordenamos los datos anteriores de mayor a menor
+
+poema_palabras_con_caracteres %>%
+  count(NumLetras, sort = TRUE)
+
+# FRECUENCIA ABSOLUTA Y FRECUENCIA RELATIVA #
+
+# Calculamos las frecuencias relativas en tantos por mil
+
+poema_frecuencias <- poema_palabras %>%
+  count(palabra, sort = T) %>%
+  mutate(relativa = 1000 * (n / sum(n))) 
+
+poema_frecuencias
+
+# PALABRAS GRAMATICALES #
+
+# Filtrar las palabras vacías
+
+vacias <- get_stopwords("es")
+
+vacias
+
+vacias <- vacias %>%
+  rename(palabra = word)
+vacias
+
+poema_vaciado <- poema_palabras_frec_abs %>%
+  anti_join(vacias)
+view(poema_vaciado)
+
+#################################################
+#                 Ejercicio 2:                  #             
+# Intenta hacer lo mismo con el Quijote de 1605 #
+#################################################
